fb6d7e8bd635ba9a00eadfbbdf43f0bdea05a568
theinfin1ty <anantgaurav1010@gmail.com>
2022-09-22 18:29:32 +0530

    Added basic crup APIs and established connection to mongodb using mongoose

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-intro'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/products/product.model.ts
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+export class Product {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public price: number,
+  ) {}
+}
